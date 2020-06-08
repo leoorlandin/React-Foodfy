5c37932f097163e1e240e4d76f55dab6f07a440a
unknown <leonardo.1117@hotmail.com>
2020-06-08 03:03:01 -0300

    inital commit for React-Foodfy

---
+++ src/components/header.js
@@ -0,0 +1,22 @@
+import { React } from 'react';
+
+const Header = () => {
+  return (
+
+
+    <header>
+      <div>
+        <a href="/"><img src="../public/logo.pgn" alt="Foodfy logo"/></a>
+      </div>
+
+      <a href="/">Sobre</a>
+      <a href="/">Receitas</a>
+    </header>
+  );
+};
+
+
+export default Header;
+
+
+
