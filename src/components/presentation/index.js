6d3a26764380c643dbc9c4ce423a6c05cf54266c
unknown <leonardo.1117@hotmail.com>
2020-06-10 08:56:07 -0300

    styling presentation component

--- src/components/presentation/index.js
+++ src/components/presentation/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import chef from './chef.png'
 
 const Presentation = () => {
   return (
-    <div>
-      DIV DE APRESENTAÇÃO
+    <div className="principal-box">
+      <div className="principal-texts">
+        <h1>As melhores receitas</h1>
+        <h2>Aprenda a construir os melhores pratos com receitas criadas por profissionais do mundo inteiro</h2>
+      </div>
+
+      <img src={chef} alt="Ilustração chef" />
+
     </div>
+
   );
 };
 
