2e329eba6b0334102894af810c32c503b567190f
unknown <leonardo.1117@hotmail.com>
2020-06-14 10:14:59 -0300

    adding static content to the RecipeList component

--- src/components/RecipeList/index.js
+++ src/components/RecipeList/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import imageBundle from '../../utils/imageBundle';
 
 const RecipeList = () => {
   return (
@@ -6,7 +7,41 @@ const RecipeList = () => {
       <h1>Mais acessadas</h1>
 
       <div className="recipes-list">
-        
+        <div className="recipe-item">
+          <img src={imageBundle.asinhas} alt="imagem da receita" />
+          <h2>Asinha</h2>
+          <h2>receita por Nome do chef</h2>
+        </div>
+
+        <div className="recipe-item">
+          <img src={imageBundle.burger} alt="imagem da receita" />
+          <h2>Burguer</h2>
+          <h2>receita por Nome do chef</h2>
+        </div>
+
+        <div className="recipe-item">
+          <img src={imageBundle.doce} alt="imagem da receita" />
+          <h2>Doce</h2>
+          <h2>receita por Nome do chef</h2>
+        </div>
+
+        <div className="recipe-item">
+          <img src={imageBundle.espaguete} alt="imagem da receita" />
+          <h2>Espaguete</h2>
+          <h2>receita por Nome do chef</h2>
+        </div>
+
+        <div className="recipe-item">
+          <img src={imageBundle.lasanha} alt="imagem da receita" />
+          <h2>Lasanha</h2>
+          <h2>receita por Nome do chef</h2>
+        </div>
+
+        <div className="recipe-item">
+          <img src={imageBundle.pizza} alt="imagem da receita" />
+          <h2>Pizza</h2>
+          <h2>receita por Nome do chef</h2>
+        </div>
       </div>
     </div>
   );
---
+++ src/utils/imageBundle.js
@@ -0,0 +1,18 @@
+import asinhas from '../assets/asinhas.png'
+import burger from '../assets/burger.png'
+import doce from '../assets/doce.png'
+import espaguete from '../assets/espaguete.png'
+import lasanha from '../assets/lasanha.png'
+import pizza from '../assets/pizza.png'
+
+const imageBundle = {
+  asinhas,
+  burger,
+  doce,
+  espaguete,
+  lasanha,
+  pizza
+}
+
+
+export default imageBundle;
